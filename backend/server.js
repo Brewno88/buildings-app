4b33d0ed34b9d74f7328bc0cbf10fa1c0e207cb2
Vincenzo <ilcontevince@gmail.com>
2020-08-02 21:41:00 +0100

    back to old structure plus added scripts

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 const mongoose = require('mongoose');
 require('dotenv/config');
 const cors = require('cors');
