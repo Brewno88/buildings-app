884e1b2d049e584cd033c908f785240910e841cf
Vincenzo <ilcontevince@gmail.com>
2020-08-04 15:58:43 +0100

    created table for buildings

--- src/api/Buildings.js
+++ src/api/Buildings.js
@@ -1,13 +1,36 @@
 import React, { useContext } from 'react';
 import { BuildContext } from '../context/GlobalContext';
-import { MyContainer } from '../components/ComponentsStyles';
+import { MyTable } from './APIStyle';
+import { Link } from 'react-router-dom';
 
 function Buildings() {
 	const { buildings } = useContext(BuildContext);
+	console.log(buildings);
 	return (
-		<MyContainer>
-			<h1>This is the Buildings CRM</h1>
-		</MyContainer>
+		<MyTable className='container'>
+			<thead>
+				<tr>
+					<th>Name</th>
+					<th>Type</th>
+					<th>Status</th>
+					<th>Location</th>
+				</tr>
+			</thead>
+			<tbody>
+				{buildings.map(build => (
+					<tr key={build._id}>
+						<td>{build.name}</td>
+						<td>{build.type}</td>
+						<td>{build.status}</td>
+						<td>{build.location}</td>
+						<td>
+							<Link to={`/buildings/edit/${build._id}`}>Edit</Link>|
+							<Link to={`/delete/${build._id}`}>Delete</Link>
+						</td>
+					</tr>
+				))}
+			</tbody>
+		</MyTable>
 	);
 }
 
