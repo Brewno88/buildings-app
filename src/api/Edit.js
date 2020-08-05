067f27a711f4e14460f8644867280991568d0c00
Vincenzo <ilcontevince@gmail.com>
2020-08-05 16:05:08 +0100

    add Update building functionalities

--- src/api/Edit.js
+++ src/api/Edit.js
@@ -1,62 +1,114 @@
-import React, { useContext, useState, useEffect } from 'react';
+import React, { useContext, useReducer, useState, useEffect } from 'react';
 import { BuildContext } from '../context/GlobalContext';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import Spinner from 'react-bootstrap/Spinner';
 import { MyForm } from './APIStyle';
+import { updateBuildReducer } from '../context/reducers';
+import axios from 'axios';
 
 function Buildings() {
 	const { buildings } = useContext(BuildContext);
-	const [details, setDetails] = useState(buildings);
+	const [id, setId] = useState('');
+	const [update, dispatchUpdate] = useReducer(updateBuildReducer, {
+		name: '',
+		type: '',
+		description: '',
+		status: '',
+		grossArea: 0,
+		location: '',
+		imageSrc: '',
+		floors: [],
+	});
+
+	const getIdFromUrl = () => {
+		const params = window.location.pathname.split('/');
+		const urlId = params[params.length - 1];
+		return urlId;
+	};
+
+	const onInputChange = (inputValue, property) => {
+		dispatchUpdate({
+			type: 'UPDATE',
+			payload: { update, inputValue, property },
+		});
+	};
+
+	const onFormSubmit = async e => {
+		e.preventDefault();
+		console.log(update);
+		axios.put(`/api/buildings/edit/${id}`, update);
+	};
 
 	useEffect(() => {
-		setDetails(buildings);
-	}, [buildings]);
+		setId(getIdFromUrl());
+		const selected = buildings.filter(building => building._id === id)[0];
+		dispatchUpdate({ type: 'SET', payload: selected });
+	}, [buildings, id]);
 
-	return details.length ? (
+	return update ? (
 		<>
-			<MyForm className='container'>
+			<MyForm className='container' onSubmit={e => onFormSubmit(e)}>
 				<Form.Group controlId='formId'>
 					<Form.Label>Building id</Form.Label>
-					<Form.Control placeholder={details[0]._id} disabled />
+					<Form.Control value={update._id} disabled />
 				</Form.Group>
 
 				<Form.Group controlId='formName'>
 					<Form.Label>Name</Form.Label>
-					<Form.Control value={details[0].name}></Form.Control>
+					<Form.Control
+						value={update.name}
+						onChange={e => onInputChange(e.target.value, 'name')}
+					></Form.Control>
 				</Form.Group>
 
 				<Form.Group controlId='formType'>
 					<Form.Label>Type</Form.Label>
-					<Form.Control value={details[0].type}></Form.Control>
+					<Form.Control
+						value={update.type}
+						onChange={e => onInputChange(e.target.value, 'type')}
+					></Form.Control>
 				</Form.Group>
 
 				<Form.Group controlId='formStatus'>
 					<Form.Label>Status</Form.Label>
-					<Form.Control value={details[0].status}></Form.Control>
+					<Form.Control
+						value={update.status}
+						onChange={e => onInputChange(e.target.value, 'status')}
+					></Form.Control>
 				</Form.Group>
 
 				<Form.Group controlId='formDescription'>
 					<Form.Label>Description</Form.Label>
 					<Form.Control
 						as='textarea'
-						value={details[0].description}
+						value={update.description}
+						onChange={e => onInputChange(e.target.value, 'description')}
 					></Form.Control>
 				</Form.Group>
 
 				<Form.Group controlId='formGrossAre'>
 					<Form.Label>GrossArea</Form.Label>
-					<Form.Control value={details[0].grossArea}></Form.Control>
+					<Form.Control
+						value={update.grossArea}
+						onChange={e => onInputChange(e.target.value, 'grossArea')}
+					></Form.Control>
 				</Form.Group>
 
 				<Form.Group controlId='formLocation'>
 					<Form.Label>Location</Form.Label>
-					<Form.Control value={details[0].location}></Form.Control>
+					<Form.Control
+						value={update.location}
+						onChange={e => onInputChange(e.target.value, 'location')}
+					></Form.Control>
 				</Form.Group>
 
 				<Form.Group controlId='formImage'>
 					<Form.Label>Image Source</Form.Label>
-					<Form.Control value={details[0].imageSrc}></Form.Control>
+					<Form.Control
+						value={update.imageSrc}
+						onChange={e => onInputChange(e.target.value, 'imageSrc')}
+					></Form.Control>
 				</Form.Group>
 
 				<Button variant='primary' type='submit'>
--- src/context/reducers.js
+++ src/context/reducers.js
@@ -2,11 +2,29 @@ const TYPE = 'TYPE';
 const STATUS = 'STATUS';
 const RESET = 'RESET';
 const SELECTED = 'SELECTED';
+const UPDATE = 'UPDATE';
+const SET = 'SET';
 
 // Selected Building from Card VIEW DETAILS button
 export const selectedReducer = (state, action) => {
 	switch (action.type) {
 		case SELECTED:
+			console.log(action.payload);
+			return action.payload;
+		default:
+			return state;
+	}
+};
+
+// Update selected Building
+export const updateBuildReducer = (state, action) => {
+	switch (action.type) {
+		case UPDATE:
+			return {
+				...action.payload.update,
+				[action.payload.property]: action.payload.inputValue,
+			};
+		case SET:
 			return action.payload;
 		default:
 			return state;
