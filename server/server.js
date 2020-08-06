368d3aa92fb0951f3af7996b95d7c2e4bd3ddfb6
Vincenzo <ilcontevince@gmail.com>
2020-08-06 15:18:38 +0100

    fixed structure and scripts

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
 const mongoose = require('mongoose');
-require('dotenv/config');
+require('dotenv').config();
 const cors = require('cors');
 const path = require('path');
 
