07c012698dcbe9c5574cb0af3b5a11cbcdf9fb00
Vincenzo <ilcontevince@gmail.com>
2020-08-06 14:58:07 +0100

    add production options

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const PORT = process.env.PORT || 5000;
 const mongoose = require('mongoose');
 require('dotenv/config');
 const cors = require('cors');
+const path = require('path');
 
 app.use(cors());
 app.use(express.json());
@@ -22,6 +23,16 @@ connection.once('open', () => {
 // Routes
 const buildingsRouter = require('./routes/buildings');
 app.use('/api/buildings', buildingsRouter);
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+	// Set a static folder
+	app.use(express.static('../build'));
+
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, '../build', 'index.html'));
+	});
+}
 
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));
 app.get('/', (req, res) => {
